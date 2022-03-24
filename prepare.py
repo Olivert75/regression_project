ed999ec9693e872773a459ac348dff24b238f585
Oliver Ton <oliver.ton75@gmail.com>
2022-03-24 18:32:46 -0500

    change some functions

--- prepare.py
+++ prepare.py
@@ -86,11 +86,48 @@ def clean_zillow_data(df):
     
     return df
     
+def split_data(df):
+    '''
+    take in a DataFrame and return train, validate, and test DataFrames.
+    
+    '''
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, 
+                                       test_size=.3, 
+                                       random_state=123)
+    print(f'train -> {train.shape}')
+    print(f'validate -> {validate.shape}')
+    print(f'test -> {test.shape}')                                  
+    return train, validate, test
 
+def split_Xy (train, validate, test, target):
+    '''
+    This function takes in three dataframe (train, validate, test) and a target  and splits each of the 3 samples
+    into a dataframe with independent variables and a series with the dependent, or target variable.
+    The function returns 3 dataframes and 3 series:
+    X_train (df) & y_train (series), X_validate & y_validate, X_test & y_test.
+    Example:
+    X_train, y_train, X_validate, y_validate, X_test, y_test = split_Xy (train, validate, test, 'Fertility' )
+    '''
+    
+    #split train
+    X_train = train.drop(columns= [target])
+    y_train= train[target]
+    #split validate
+    X_validate = validate.drop(columns= [target])
+    y_validate= validate[target]
+    #split validate
+    X_test = test.drop(columns= [target])
+    y_test= test[target]
+
+    print(f'X_train -> {X_train.shape}               y_train->{y_train.shape}')
+    print(f'X_validate -> {X_validate.shape}         y_validate->{y_validate.shape} ')        
+    print(f'X_test -> {X_test.shape}                  y_test>{y_test.shape}') 
+    return  X_train, y_train, X_validate, y_validate, X_test, y_test
 
 def train_validate_test(df, target):
     '''
-    this function takes in a dataframe and splits it into 3 samples, 
+    this function combine from 2 functions above, 
     a test, which is 20% of the entire dataframe, 
     a validate, which is 24% of the entire dataframe,
     and a train, which is 56% of the entire dataframe. 
