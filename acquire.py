a2de4f57356418728aed28a921378a62c2896913
Oliver Ton <oliver.ton75@gmail.com>
2022-03-25 13:07:06 -0500

    delete some function in acquire

--- acquire.py
+++ acquire.py
@@ -40,37 +40,3 @@ def get_zillow_data():
     return df
     
     
-    
-    
-def clean_zillow(df):
-    '''
-    this function takes in an unclean zillow df and does the following:
-    1.) keeps only columns we need for the project
-    2.) drops nulls
-    3.) renames columns
-    '''
-    #select features for df, took these features from my acquire exercise
-    features = ['parcelid', 'calculatedfinishedsquarefeet', 'bathroomcnt', 'bedroomcnt', 'taxvaluedollarcnt','yearbuilt','taxamount','fips']
-    df = df[features]
-
-    
-    #rename columns for easier use
-    df = df.rename(columns={
-                            'parcelid': 'parcel_id',
-                            'calculatedfinishedsquarefeet': 'sqft',
-                            'bathroomcnt': 'baths',
-                            'bedroomcnt': 'beds',
-                            'taxvaluedollarcnt':'tax_value',
-                            'yearbuilt':'year_built',
-                            'taxamount': 'tax_amount'
-        
-    })
-    
-    #set index
-    df = df.set_index('parcel_id')
-    #drop nulls
-    df = df.dropna(subset=['sqft','tax_value'])
-    
-    return df
-    
-    
